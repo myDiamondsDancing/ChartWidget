277b2add2528f8e54f6aee5e7dbe7738a8723375
Dmitriy <38916727+myDiamondsDancing@users.noreply.github.com>
2018-10-27 12:03:46 +0300

    Update chartWidget.py

--- chartWidget.py
+++ chartWidget.py
@@ -19,7 +19,7 @@ class ChartWidget(QWidget):
         self.setTheme(self.theme)
         
     def initUi(self):
-    '''private method : inits user interface'''
+        '''private method : inits user interface'''
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure) 
         self.toolBar = NavigationToolbar(self.canvas, self)
@@ -50,17 +50,17 @@ class ChartWidget(QWidget):
         self.setLayout(self.mainLayout)
         
     def loadFields(self):
-    '''|loadFields(self)
+        '''|loadFields(self)
        |private method : loads local field(theme)'''
         self.theme = 0     
         
     def connectUi(self):
-    '''private method : connects all needed signals with class methods'''
+        '''private method : connects all needed signals with class methods'''
         self.addButton.clicked.connect(self.area.addWidget)
         self.plotButton.clicked.connect(self.plot)
         
     def plot(self):
-    '''|plot(self)
+        '''|plot(self)
        |public method : plots chart is canvas using chartSettingsWidget.results
        |plot >>> True if plotting is success else False '''
         self.figure.clear()
@@ -84,7 +84,7 @@ class ChartWidget(QWidget):
         return True            
         
     def setTheme(self, flag):
-    '''|setTheme(self, flag)
+        '''|setTheme(self, flag)
        |flag : (0: Light, 1: Gray, 2: Dark) 
        |public method : sets theme for widgets
        |setTheme >>> None'''
@@ -97,7 +97,7 @@ class ChartWidget(QWidget):
             self.setDarkTheme()        
 
     def setLigthTheme(self):
-    '''|setLigthTheme(self)
+        '''|setLigthTheme(self)
        |private method : sets light theme for widgets'''
        
         self.setStyleSheet("background : rgb(238, 238, 238);")
@@ -110,16 +110,18 @@ class ChartWidget(QWidget):
         self.area.setTheme(1)
 
     def setDarkTheme(self):
-    '''|setGrayTheme(self)
+        '''|setGrayTheme(self)
        |private method : sets dark theme for widget''' 
         self.setStyleSheet("background : rgb(103, 103, 103);")
         self.area.setTheme(2)  
         
     def showDialog(self, text):
-    '''|showDialog(self, text)
+        '''|showDialog(self, text)
        |text : String
        |private method : shows error dialog with input text'''         
         dlg = QMessageBox(self)
         dlg.setText(text)
         dlg.setIcon(QMessageBox.Critical)
         dlg.show()            
+            
+       
